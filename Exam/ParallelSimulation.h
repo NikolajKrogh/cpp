a2d392b9592c289e10fec9ef80b97d83a4a6b711
krogh <nkrogh20@student.aau.dk>
2024-06-13 21:36:45 +0200

    parallel does not create a new file unless it also writes to file

--- Exam/ParallelSimulation.h
+++ Exam/ParallelSimulation.h
@@ -8,10 +8,12 @@ namespace stochastic {
         template<class Observer>
         static void parallelize_simulations(std::vector<Vessel> vessels, Observer observer, double simulation_time, bool output_to_file = false) {
             std::vector<std::thread> threads;
-
+            std::string path;
             // For each vessel, assign a unique filename and start a simulation in a new thread.
             for (auto &v: vessels) {
-                std::string path = Simulation::Simulation::assign_unique_filename(v.name);
+                if (output_to_file) {
+                    path = Simulation::Simulation::assign_unique_filename(v.name);
+                }
                 threads.push_back(std::thread(Simulation::simulate<Observer>, path, std::ref(v), observer, simulation_time, output_to_file));
             }
 
